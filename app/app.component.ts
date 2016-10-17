c6613647bee022a8dc40faa10fabd6b58f45ad9c
Per Johannesson <per.johannesson@transpa.se>
2016-10-17 08:06:32 +0200

    Lite goa ändringar

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    templateUrl:'app/app.component.html'    
 })
-export class AppComponent { }
+export class AppComponent { 
+    //[ ] means property binding - Component to DOM
+    //( ) means event binding - DOM to Component
+    title = 'Customer App';
+    name = 'Perry';
+    perrysColor = 'blue';
+
+    changeSuitColor(){
+        this.perrysColor = this.perrysColor=== 'blue' ? 'red':'blue';
+    }
+
+}
